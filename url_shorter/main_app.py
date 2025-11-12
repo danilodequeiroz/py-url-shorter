5d902f1e1b2ce1d8658feca6d72ce4311ae0cb3c
danilodequeiroz <danilo.aq@gmail.com>
2025-11-12 11:40:42 -0300

    initial commit

---
+++ url_shorter/main_app.py
@@ -0,0 +1,14 @@
+import  base62
+from url_shorter.url_shorter_base_64 import UrlShorterBase64
+
+
+def main():
+    url_shorter = UrlShorterBase64()
+    url_shorter.long_url = "https://google.com"
+    url_shorter.short_url = "https://goo.gl"
+    print(f"long url {url_shorter.long_url}")
+    print(f"short url {url_shorter.short_url}")
+    print("short url: {} long url: {}".format(url_shorter.short_url, url_shorter.long_url) )
+
+if __name__ == "__main__":
+    main()
